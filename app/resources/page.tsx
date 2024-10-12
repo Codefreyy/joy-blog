8c5ab6bb6a9560afb447a9030086e6743526b073
Yujie <yp25@st-andrews.ac.uk>
2024-10-12 19:32:42 +0100

    feat: add sources section to navbar

---
+++ app/resources/page.tsx
@@ -0,0 +1,53 @@
+import readingsData from '@/data/readingsData'
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'Readings' })
+
+export default function Resources() {
+  return (
+    <>
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <p className="text-md leading-7 text-gray-500 dark:text-gray-400">
+          我扑在书上，就像饥饿的人扑在面包上。
+        </p>
+        <p className="text-md leading-7 text-gray-500 dark:text-gray-400">
+          以下是我学过的、正在学习的或想学的好资源，推荐给大家。
+        </p>
+      </div>
+      <ReadingSection readingsData={readingsData} />
+    </>
+  )
+}
+
+function ReadingSection({ readingsData }) {
+  return (
+    <>
+      {Object.keys(readingsData).map((key, index) => {
+        return (
+          <div key={readingsData[key].title} className="container pt-5">
+            <h1 className="text-lg font-bold">{key}</h1>
+            <ol className="mt-5 space-y-2">
+              {readingsData[key].map((item, index) => {
+                return (
+                  <li key={item.title}>
+                    <a
+                      href={item.href}
+                      target="_blank"
+                      className={
+                        item.href
+                          ? 'no-underline hover:text-yellow-600 hover:underline hover:underline-offset-4'
+                          : ''
+                      }
+                    >
+                      {index + 1}. {item.title}
+                    </a>
+                  </li>
+                )
+              })}
+            </ol>
+          </div>
+        )
+      })}
+    </>
+  )
+}
---
+++ data/readingsData.ts
@@ -0,0 +1,66 @@
+const readingsData = {
+  Javascript: [
+    {
+      title: 'JavaScript: The Hard Parts, v2 - Frontend Masters',
+      status: 'done',
+      href: 'https://frontendmasters.com/courses/javascript-hard-parts-v2/',
+    },
+    { title: '你不知道的JavaScript（上卷）', status: 'done' },
+    { title: '你不知道的JavaScript（中卷）', status: 'done' },
+  ],
+  性能优化: [
+    {
+      title: 'JavaScript Performance - Frontend Masters',
+      href: 'https://frontendmasters.com/courses/web-performance/',
+    },
+    {
+      title: 'Web Performance Tips⚡️',
+      href: 'https://webperf.tips',
+    },
+    {
+      title: '前端性能优化原理与实践 - 掘金',
+      href: 'https://juejin.cn/book/6844733750048210957?enter_from=course_center&utm_source=course_center',
+    },
+  ],
+  浏览器: [
+    {
+      title: '浏览器工作原理与实践 - 极客时间',
+      href: 'https://time.geekbang.org/column/intro/100033601',
+    },
+  ],
+  设计模式: [
+    {
+      title: 'JavaScript 设计模式核心原理与应用实践 - 掘金',
+      href: 'https://juejin.cn/book/6844733790204461070?enter_from=course_center',
+    },
+  ],
+  数据结构与算法: [
+    {
+      title: '前端算法与数据结构面试：底层逻辑解读与大厂真题训练',
+      href: 'https://juejin.cn/book/6844733800300150797?enter_from=course_center&utm_source=course_center',
+    },
+    {
+      title: "The Last Algorithm Class You'll Ever Need - Frontend Masters",
+      href: 'https://frontendmasters.com/courses/algorithms/',
+    },
+  ],
+  'Node.js': [
+    {
+      title: 'Understanding Node.js: Core Concepts - Udemy',
+      href: 'https://www.udemy.com/course/understanding-nodejs-core-concepts/',
+    },
+  ],
+  其他: [
+    {
+      title: '我看过的Youtube高质量视频',
+      href: 'https://www.youtube.com/playlist?list=PLtc4S9-a2fprxWMv_ZR0ggxJOdXAT_Gd5/',
+      status: 'done',
+    },
+    {
+      title: '面试现场 - 极客时间',
+      href: 'https://time.geekbang.org/column/intro/100023401',
+    },
+  ],
+}
+
+export default readingsData
